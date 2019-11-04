e5895e18984187c28aea74ddeeeb21a491529403
Quin Adam <quin.j.adam@gmail.com>
2019-11-04 15:18:41 -0800

    Created an example circular progress bar for use later

--- app/src/main/java/com/example/searchparty/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/searchparty/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.support.annotation.Nullable;
 import android.support.annotation.NonNull;
@@ -19,9 +21,12 @@ public class HomeFragment extends Fragment {
     
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        //initialize crucial variables
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        
+        //text view live data observer example
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -29,6 +34,24 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        
+        //circular progress bar test
+        final Button testBtn = root.findViewById(R.id.homeTestBtn);
+        final ProgressBar circleProgBar = root.findViewById(R.id.homeTestProgBar);
+        final TextView progTxt = root.findViewById(R.id.homeTestProgTxt);
+        
+        circleProgBar.setProgress(10);
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer currProg = circleProgBar.getProgress();
+                Integer newProg = currProg>=100?10:currProg+10;
+                circleProgBar.setProgress(newProg);
+                progTxt.setText(newProg.toString()+"%");
+                
+            }
+        });
+        
         return root;
     }
 }
