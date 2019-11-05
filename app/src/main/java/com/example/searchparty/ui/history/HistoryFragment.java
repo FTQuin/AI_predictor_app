fdbd9bd4211d7f20f3d2bfe426ae24aa61d436f9
Quin Adam <quin.j.adam@gmail.com>
2019-11-04 18:51:33 -0800

    renamed all references of gallery fragment to history fragment

--- app/src/main/java/com/example/searchparty/ui/history/HistoryFragment.java
+++ app/src/main/java/com/example/searchparty/ui/history/HistoryFragment.java
@@ -1,4 +1,4 @@
-package com.example.searchparty.ui.gallery;
+package com.example.searchparty.ui.history;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,17 +13,17 @@ import android.arch.lifecycle.ViewModelProviders;
 
 import com.example.searchparty.R;
 
-public class GalleryFragment extends Fragment {
+public class HistoryFragment extends Fragment {
     
-    private GalleryViewModel galleryViewModel;
+    private HistoryViewModel historyViewModel;
     
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        historyViewModel =
+                ViewModelProviders.of(this).get(HistoryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_history, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
+        historyViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/searchparty/ui/history/HistoryViewModel.java
+++ app/src/main/java/com/example/searchparty/ui/history/HistoryViewModel.java
@@ -1,16 +1,16 @@
-package com.example.searchparty.ui.gallery;
+package com.example.searchparty.ui.history;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class HistoryViewModel extends ViewModel {
     
     private MutableLiveData<String> mText;
     
-    public GalleryViewModel() {
+    public HistoryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is history fragment");
     }
     
     public LiveData<String> getText() {
