9e8c1c4066b323e7d23ae22ec7703e0077f3ce16
Quin Adam <quin.j.adam@gmail.com>
2019-11-04 18:14:30 -0800

    Added Database interface class and tested it in HomeFragment

--- app/src/main/java/com/example/searchparty/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/searchparty/ui/home/HomeFragment.java
@@ -1,11 +1,11 @@
 package com.example.searchparty.ui.home;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.support.annotation.Nullable;
 import android.support.annotation.NonNull;
@@ -13,6 +13,7 @@ import android.support.v4.app.Fragment;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 
+import com.example.searchparty.DatabaseInterfaces.DatabaseInterface;
 import com.example.searchparty.R;
 
 public class HomeFragment extends Fragment {
@@ -35,20 +36,39 @@ public class HomeFragment extends Fragment {
             }
         });
         
-        //circular progress bar test
-        final Button testBtn = root.findViewById(R.id.homeTestBtn);
-        final ProgressBar circleProgBar = root.findViewById(R.id.homeTestProgBar);
-        final TextView progTxt = root.findViewById(R.id.homeTestProgTxt);
+        //test button
+        final Button testBtn1 = root.findViewById(R.id.homeTestBtn1);
+        final Button testBtn2 = root.findViewById(R.id.homeTestBtn2);
         
-        circleProgBar.setProgress(10);
-        testBtn.setOnClickListener(new View.OnClickListener() {
+        //circular progress bar test
+//        final ProgressBar circleProgBar = root.findViewById(R.id.homeTestProgBar);
+        final TextView progTxt = root.findViewById(R.id.homeTestProgTxt);
+//        circleProgBar.setProgress(10);
+        
+        //test databaseInterface
+        final DatabaseInterface dbi = new DatabaseInterface(this.getContext());
+    
+        testBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Integer currProg = circleProgBar.getProgress();
-                Integer newProg = currProg>=100?10:currProg+10;
-                circleProgBar.setProgress(newProg);
-                progTxt.setText(newProg.toString()+"%");
+                //add to progress bar
+//                Integer newProg = (circleProgBar.getProgress()+10)%100;
+//                circleProgBar.setProgress(newProg);
+//                progTxt.setText(newProg.toString()+"%");
+            
+                // test database
+                dbi.addData("400%");
                 
+            }
+        });
+        testBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            
+                // test database
+                Cursor data = dbi.getData();
+                data.moveToNext();
+                progTxt.setText(data.getString(1));
             }
         });
         
