b5bf4fe3b68bb8c99f5346a7e77f60ab28c06160
Quin Adam <quin.j.adam@gmail.com>
2019-11-14 20:47:42 -0800

    get everything in order to migrate to androidx

--- app/src/main/java/com/example/searchparty/MainActivity.java
+++ app/src/main/java/com/example/searchparty/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.searchparty;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -19,13 +22,14 @@ import android.view.Menu;
 
 import com.example.searchparty.Models.Game;
 import com.example.searchparty.Models.Prediction;
-import com.example.searchparty.dummy.DummyContent;
 import com.example.searchparty.ui.future_games.FutureGamesFragment;
+import com.example.searchparty.ui.predict_game.PredictGameFragment;
 import com.example.searchparty.ui.saved_prediction.SavedPredictionFragment;
 
 public class MainActivity extends AppCompatActivity implements SavedPredictionFragment.OnListFragmentInteractionListener, FutureGamesFragment.OnListFragmentInteractionListener {
     
     private AppBarConfiguration mAppBarConfiguration;
+    private FragmentManager fm;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +49,10 @@ public class MainActivity extends AppCompatActivity implements SavedPredictionFr
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        
+        
+    
+        fm = getSupportFragmentManager();
     }
     
     @Override
@@ -61,13 +69,28 @@ public class MainActivity extends AppCompatActivity implements SavedPredictionFr
                 || super.onSupportNavigateUp();
     }
     
+    private void changeFragment(Game game){
+        Navigation.findNavController(findViewById(R.id.nav_host_fragment)).navigate(R.id.action_nav_future_games_to_nav_predict_game);
+        final FragmentTransaction ft = fm.beginTransaction();
+        Fragment frag = new PredictGameFragment();
+        Bundle gameBundle = new Bundle();
+        gameBundle.putString("gameID", game.getID());
+        frag.setArguments(gameBundle);
+        ft.replace(R.id.nav_host_fragment, frag);
+        ft.addToBackStack("frags");
+        ft.commit();
+    }
+    
+    
+    
     @Override
     public void onListFragmentInteraction(Prediction prediction) {
     
     }
     
+    //called when a game is clicked on in future games
     @Override
-    public void onListFragmentInteraction(Game item) {
-    
+    public void onListFragmentInteraction(Game game) {
+        changeFragment(game);
     }
 }
