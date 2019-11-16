e0d1e9930df653dbc5f504cbcede45bed43f4706
Quin Adam <quin.j.adam@gmail.com>
2019-11-16 10:03:25 -0800

    started adding functionality to outcome fragment

--- app/src/main/java/com/example/searchparty/ui/outcome/OutcomeFragment.java
+++ app/src/main/java/com/example/searchparty/ui/outcome/OutcomeFragment.java
@@ -22,13 +22,13 @@ public class OutcomeFragment extends Fragment {
         outcomeViewModel =
                 ViewModelProviders.of(this).get(OutcomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_outcome, container, false);
-        final TextView textView = root.findViewById(R.id.text_outcome);
-        outcomeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_outcome);
+//        outcomeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
